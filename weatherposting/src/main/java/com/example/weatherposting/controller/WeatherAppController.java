f60c861037ecafaa00c6ff6ebedd423465e7e566
matthewrockwood <rockwoodmatthew@gmail.com>
2025-11-23 17:34:25 -0500

    y

---
+++ weatherposting/src/main/java/com/example/weatherposting/controller/WeatherAppController.java
@@ -0,0 +1,29 @@
+package com.example.weatherposting.controller;
+
+import com.example.weatherposting.model.WeatherEntry;
+import com.example.weatherposting.service.WeatherService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/weather")
+@RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:3000")
+public class WeatherAppController {
+
+    private WeatherService service;
+    @PostMapping
+    public WeatherEntry postWeather(@RequestParam String city, @RequestParam String msg){
+        return service.addEntry(city, msg);
+    }
+    @GetMapping
+    public List<WeatherEntry> getWeather(){
+        return service.getAllEntries();
+    }
+    @GetMapping("/{city}")
+    public List<WeatherEntry> getWeather(@PathVariable String city){
+        return service.getEntriesByCity(city);
+    }
+}
---
+++ weatherposting/src/main/java/com/example/weatherposting/service/WeatherService.java
@@ -0,0 +1,21 @@
+package com.example.weatherposting.service;
+
+import com.example.weatherposting.model.WeatherEntry;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class WeatherService {
+    public WeatherEntry addEntry(String city, String msg) {
+        return null;
+    }
+
+    public List<WeatherEntry> getEntriesByCity(String city) {
+        return null;
+    }
+
+    public List<WeatherEntry> getAllEntries() {
+        return null;
+    }
+}
